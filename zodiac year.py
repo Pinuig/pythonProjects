8ed27538125f13d1557f23f604a5eb56f6a3e168
Pinuig <116064408+Pinuig@users.noreply.github.com>
2024-11-28 17:49:44 -0800

    Add files via upload
    Chinese Zodiac Year Calculator
    This Python project calculates the Chinese zodiac year for a given year. The Chinese zodiac is a repeating cycle of 12 years, with each year being represented by an animal and its reputed attributes.
    How to Use
    Run the Program: Execute the Python script to start the calculator.
    Input the Year: Enter the year for which you want to find the zodiac sign.
    Get the Result: The program will display the corresponding Chinese zodiac animal for that year.

---
+++ zodiac year.py
@@ -0,0 +1,41 @@
+year = eval(input("enter your year here: "))
+zodiac_year = year % 12
+
+if zodiac_year == 1:
+    print("your zodiac is Rat")
+    
+elif zodiac_year == 2:
+    print("your zodiac is Cow")
+    
+elif zodiac_year == 3:
+    print('your zodiac is tiger')
+     
+elif zodiac_year == 4:
+    print("your zodiac is rabbit")
+    
+elif zodiac_year == 5:
+    print('your zodiac is Dragon')
+    
+elif zodiac_year == 6:
+    print("your zodiac is snake")
+    
+elif zodiac_year == 7:
+    print('your zodiac is horse')
+    
+elif zodiac_year == 8:
+    print("your zodiac is goat")
+    
+elif zodiac_year == 9:
+    print('your zodiac is Monkey')
+    
+elif zodiac_year == 10:
+    print("your zodiac is Rooster")
+    
+elif zodiac_year == 11:
+    print("your zodiac is Dog")
+    
+elif zodiac_year == 12:
+    print('your zodiac is Pig')
+    
+else: 
+    print("you're not having a zodiac year")
